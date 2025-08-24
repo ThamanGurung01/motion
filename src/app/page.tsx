d6ee87f3f00fa2a31dc1690495bcd4a8818b2813
ThamanGurung01 <tgurung519@gmail.com>
2025-08-24 17:57:45 +0545

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-import Marquee from "@/components/Marquee";
+import Marquee from "@/components/MarqueeContainer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-    <section className="w-full h-screen">
+    <header className="fixed">
     <Marquee/>
     <Navbar/>
+    </header>
+    <main className="pt-24">
+    <section className="w-full min-h-screen">
     <h1>Home</h1>
     </section>
+    </main>
+    <footer></footer>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='border w-full min-h-14 grid grid-cols-[0.5fr_1fr] place-items-center'>
-    <h1>Navbar</h1>
-    <ul className='flex'>
-        <li>Home</li>
-        <li>iCaur 03</li>
-        <li>Contact us</li>
-        <li>News & Events</li>
+    <div className='w-screen overflow-x-hidden min-h-12 grid grid-cols-[0.7fr_1fr] gap-[25rem] backdrop-blur-2xl bg-black place-items-center text-white text-lg'>
+    <h1 className='font-bold italic text-xl'><a href="/">Navbar</a></h1>
+    <nav className='w-3/5'>
+    <ul className='font-normal flex items-center justify-around'>
+        <li className='font-bold'><a href="#">Home</a></li>
+        <li><a href="#">iCaur 03</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">News & Events</a></li>
     </ul>
+    </nav>
     </div>
   )
 }
